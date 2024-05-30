b5a4a4b243469f2765082df52522176be94d1742
KiriRainCat <2287990465@qq.com>
2024-05-30 17:44:21 +0800

    🎈 perf: 优化 css & js 文件大小

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "./pkg/router";
 
 // Varlet UI
-import Varlet, { Snackbar, Locale } from "@varlet/ui";
+import { Snackbar, Locale, Button, Loading, Paper, Table, Dialog, Input } from "@varlet/ui";
 import "@varlet/ui/es/style";
 import "@varlet/touch-emulator";
 Snackbar.allowMultiple(true);
@@ -14,4 +14,4 @@ Locale.use("en-US");
 // 全局样式
 import "./assets/styles.scss";
 
-createApp(App).use(router).use(Varlet).mount("#app");
+createApp(App).use(router).use(Button).use(Loading).use(Paper).use(Table).use(Dialog).use(Input).mount("#app");
