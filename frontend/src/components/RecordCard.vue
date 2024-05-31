04e1d3e2fb759bcb6749a9d70f1798815bc2bda8
KiriRainCat <2287990465@qq.com>
2024-05-31 10:41:24 +0800

    🐞 fix: 修复 table 的使用方式，让字段对其

--- frontend/src/components/RecordCard.vue
+++ frontend/src/components/RecordCard.vue
@@ -6,10 +6,10 @@ defineProps({ record: Object as PropType<Record>, onlyData: Boolean });
 </script>
 
 <template>
-  <var-paper elevation="1" radius="6" class="flex gap-6 p-3 bg-cyan-50">
-    <div v-if="!onlyData">{{ record!.name }}</div>
-    <div>{{ (record!.accuracy * 100).toFixed(0) }}%</div>
-    <div>{{ record!.time_taken.toFixed(1) }}s</div>
-    <div v-if="!onlyData">{{ new Date(record!.updated_at!).toLocaleString() }}</div>
-  </var-paper>
+  <tr>
+    <td v-if="!onlyData">{{ record!.name }}</td>
+    <td>{{ (record!.accuracy * 100).toFixed(0) }}%</td>
+    <td>{{ record!.time_taken.toFixed(1) }}s</td>
+    <td v-if="!onlyData">{{ new Date(record!.updated_at!).toLocaleString() }}</td>
+  </tr>
 </template>
--- frontend/src/views/LeaderboardView.vue
+++ frontend/src/views/LeaderboardView.vue
@@ -18,27 +18,25 @@ onBeforeUnmount(() => clearInterval(autoFetchInterval));
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center px-6">
     <div class="text-2xl font-bold mt-6">Leaderboard</div>
     <div class="mb-6 text-xs opacity-50">Data will auto refetch every 10 seconds</div>
 
     <!-- 加载动画 -->
     <var-loading v-if="records.length == 0" />
 
-    <!-- 排行榜主题 -->
-    <div v-else>
-      <div class="flex mb-1.5 font-bold">
-        <div class="ml-3 mr-[4.2rem]">Name</div>
-        <div class="mr-[1.3rem]">Acc.</div>
-        <div class="mr-[2.6rem]">Time</div>
-        <div>Recorded On</div>
-      </div>
-      <var-table elevation="0" scroller-height="76vh" class="w-[40rem]">
-        <template #default class>
-          <RecordCard v-for="record in records" :key="record.name" :record="record" class="mb-1.5" />
-        </template>
-      </var-table>
-    </div>
+    <!-- 排行榜主体 -->
+    <var-table v-else elevation="0" scroller-height="76vh" class="max-w-[46rem]">
+      <thead class="font-bold">
+        <tr>
+          <td>Name</td>
+          <td>Acc.</td>
+          <td>Time</td>
+          <td>Recorded On</td>
+        </tr>
+      </thead>
+      <RecordCard v-for="record in records" :key="record.name" :record="record" class="mb-1.5" />
+    </var-table>
 
     <!-- 返回按钮 -->
     <var-button type="primary" @click="() => $router.back()" class="absolute bottom-4">Back</var-button>
